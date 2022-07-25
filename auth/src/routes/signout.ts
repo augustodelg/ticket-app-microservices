30d198432500b127d0c4fe3fa1747c6aa5b979c2
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-07-25 18:18:34 -0300

    Arreglo controlador del signout

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,7 @@ import cookieSession from "cookie-session";
 
 const router = express.Router();
 
-router.get("/api/users/signout", (req, res) => {
+router.post("/api/users/signout", (req, res) => {
     req.session = null;
     res.send({});
 });
