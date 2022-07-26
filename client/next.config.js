ee36d8a90c1f8e94df95c062884fb09c3154c00f
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-07-26 18:28:15 -0300

    Modifico Next.config

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions.poll = 300;
+    return config;
+  }
 }
 
 module.exports = nextConfig
