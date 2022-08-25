516ef7839b3a12807af87f50b4a2931091213d59
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-08-25 13:46:40 -0300

    TICKETS: Final tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -9,6 +9,7 @@ declare global {
 }
 
 
+jest.mock('../natsWrapper');
 
 let mongo: any;
 
@@ -24,6 +25,7 @@ beforeAll(async () => {
 
 
 beforeEach(async () => {
+    jest.clearAllMocks()
     const collections = await mongoose.connection.db.collections();
 
     for (let collection of collections) {
