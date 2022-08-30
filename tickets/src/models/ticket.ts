c43163f5988f98f71558d55a5ac36f77875dcd9b
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-08-30 11:54:35 -0300

    TICKETS: Model updated and optimisticConcurrency system added

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface TicketAttrs {
     title: string;
@@ -10,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -36,7 +38,10 @@ const ticketSchema = new mongoose.Schema({
             delete ret._id;
         }
     }
-})
+});
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
