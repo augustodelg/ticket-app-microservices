fc8913895edfcdfc146a3b80f271a70fc711f125
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-09-16 11:39:30 -0300

    ORDERS: Added Listener for payments

--- services/orders/src/events/listeners/expirationCompleteListener.ts
+++ services/orders/src/events/listeners/expirationCompleteListener.ts
@@ -13,6 +13,10 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
         if (!order) {
             throw new Error('Order not found')
         }
+
+        if (order.status === OrderStatus.Complete){
+            return msg.ack();
+        }
         order.set({
             status: OrderStatus.Cancelled,
         })
---
+++ services/orders/src/events/listeners/paymentCreatedListener.ts
@@ -0,0 +1,26 @@
+import { Listener, OrderStatus, PaymentCreatedEvent, Subjects } from "@tacket/common";
+import { Message } from "node-nats-streaming";
+import OrderService from "../../services/orderService";
+import { queueGroupName } from "./queueGroupName";
+
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent>{
+    queueGroupName= queueGroupName;
+    readonly subject = Subjects.PaymentCreated;
+
+    async onMessage(data: PaymentCreatedEvent['data'], msg: Message): Promise<void> {
+        const order = await OrderService.findById(data.orderId);
+
+        if (!order) {
+            throw new Error('Order Not Found');
+        }
+
+        order.set({
+            status: OrderStatus.Complete
+        });
+
+        await order.save();
+
+        msg.ack();
+    }
+}
