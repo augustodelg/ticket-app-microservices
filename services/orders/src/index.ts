05e36d6237db6674aa3d9323f0f369d0b34ebd94
Augusto Del Grosso <adg.prodlp@gmail.com>
2022-09-16 12:23:41 -0300

    ORDERS: Instanced PaymentCreatedListener

--- services/orders/src/index.ts
+++ services/orders/src/index.ts
@@ -4,6 +4,7 @@ import { natsWrapper } from './natsWrapper';
 import { TicketCreatedListener } from './events/listeners/ticketCreatedListener';
 import { TicketUpdatedListener } from './events/listeners/ticketUpdatedListener';
 import { ExpirationCompleteListener } from './events/listeners/expirationCompleteListener';
+import { PaymentCreatedListener } from './events/listeners/paymentCreatedListener';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -37,6 +38,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
     
     await mongoose.connect(process.env.MONGO_URI);
